fcc27b1a5a7de76504b08b5ca95c72a03156a7af
Yash Chatim <yash.chatim@gmail.com>
2021-09-15 18:54:10 +0100

    Added MealItemForm component

--- src/components/Meals/MealItem/MealItem.js
+++ src/components/Meals/MealItem/MealItem.js
@@ -1,12 +1,17 @@
+import MealItemForm from './MealItemForm';
+
 import classes from './MealItem.module.css';
 
-const MealItem = props=> {
+const MealItem = props => {
     return (
         <div className={classes.meal}>
             <div>
                 <h2>{props.name}</h2>
                 <p className={classes.description}>{props.description}</p>
                 <p className={classes.price}>£{props.price}</p>
+            </div>
+            <div>
+                <MealItemForm />
             </div>
         </div>
     );
